200b404afb457bf1a9cae673eec9022dadf0eaed
Exzh34 <61991353+Exzh34@users.noreply.github.com>
2021-05-05 18:50:33 +0100

    Create Solidity simple arrays

---
+++ Solidity simple arrays.sol
@@ -0,0 +1,19 @@
+// Variables
+// Data Types
+// Custom Data Structures
+pragma solidity ^0.8.1;
+
+contract MyContract{
+   // array
+   uint[] public uintArray = [1,2,3];
+   string[] public stringArray = ["apple","banana","carrot"];
+   string[] public values;
+   uint[][] public array2D = [[1,2,3], [4,5,6]]; // two dimensional array 
+   function addValue(string memory _value) public { // "_" represents the value that has to be passed in
+       values.push(_value);  // push takes one argument and adds it to end of the array in this case to the values array
+   }
+   function valueCount() public view returns (uint){
+       return values.length;
+   }
+
+}
