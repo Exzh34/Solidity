78b6248cb5f5c4a6da42c9bb0741f4e10c3c8ef1
Exzh34 <61991353+Exzh34@users.noreply.github.com>
2021-05-08 10:35:06 +0100

    Update and rename zombie gen.sol to zombiefactory.sol

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -14,9 +14,13 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-     function _createZombie(string memory _name, uint _dna) private {
-        // and fire it here
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -26,8 +30,10 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 
 }
+
