af189cd10ca0340001bddf4ed7670d6599466eea
Exzh34 <61991353+Exzh34@users.noreply.github.com>
2021-05-11 10:49:09 +0100

    Update zombiefeeding.sol

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -38,7 +38,7 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now); 
   }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf { // function for feeding the zombie on cryptokittie
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) { // function for feeding the zombie on cryptokittie
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie)); // check if zombie is ready to be fed 
     _targetDna = _targetDna % dnaModulus; // checks for 16 integer dna count 
