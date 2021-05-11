674e0686591ea7338f43e5fd67421c5fa447b4f7
Exzh34 <61991353+Exzh34@users.noreply.github.com>
2021-05-11 10:42:25 +0100

    Update zombieattack.sol

--- zombieattack.sol
+++ zombieattack.sol
@@ -5,7 +5,7 @@ import "./zombiehelper.sol";
 contract ZombieAttack is ZombieHelper {
   // Start here
   uint randNonce = 0;
-  uint attackVictoryProbability = 70 // victory probability to 70%
+  uint attackVictoryProbability = 70; // victory probability to 70%
   function randMod(uint _modulus) internal returns(uint) { // function to generate a random number
     randNonce++; // increments randNonce 
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus; // calculates uint typecast of the keccak256 hash
